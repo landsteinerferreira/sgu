62871846fa94bd1f3754bed0e8e792417b2169d7
“landsteiner” <landsteinerferreira@hotmail.com>
2026-03-29 10:39:00 -0300

    configuring a PostgreSQL database

--- app/settings.py
+++ app/settings.py
@@ -66,8 +66,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #  Usando postgresql
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'complaints', #  Nome do banco
+        'USER': 'postgres', #  Usuario
+        'PASSWORD': 'Lands@a627ha', #  Senha
+        'HOST': 'localhost', #  Local so servidor
+        'PORT': '5432', # Porta
+        #  Usando o sqlite3
+        #'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -107,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
